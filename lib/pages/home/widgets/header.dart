c3b45514067a093aef9f82921221d55ecd4cfd82
KARBOU Aristide <aristidetiger12@gmail.com>
2023-03-15 15:42:54 +0000

    header finish

---
+++ lib/pages/home/widgets/header.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class HeaderSection extends StatelessWidget {
+  const HeaderSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        top: MediaQuery.of(context).padding.top,
+        left: 25,
+        right: 25,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text(
+                'Welcome,',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                'What would you like to play?',
+                style: TextStyle(
+                  fontSize: 19,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
+          CircleAvatar(
+            child: Image.asset(
+              'assets/images/avatar.png',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
